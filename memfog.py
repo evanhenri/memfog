f4e8a1c5f9d47d48547048e581509c0a8e50565d
evanhenri <nonce@protonmail.ch>
2016-01-26 14:05:04 -0500

    Updated cli option name, renamed variables, set title value from user input if creating new memory

--- memfog.py
+++ memfog.py
@@ -1,12 +1,12 @@
 """memfog
 Usage:
-  run.py <keyword>...[--add | --rm | --edit]
+  memfog.py [<keyword>...] [--add | --remove | --edit]
 
 Options:
   -h --help     Show this screen
   -v --version  Show version
   -a --add      Create new memory record
-  -r --rm       List records containing keywords and remove selected
+  -r --remove   List records containing keywords and remove selected
   -e --edit     List records containing keywords and edit details of selected
 """
 from docopt import docopt
@@ -40,78 +40,86 @@ def standardize(s):
 
 class Brain:
     def __init__(self):
-        self.memory_ids = {0}
+        self.memory_keys = {0}
         self.memories = {}
         self.altered = False
 
-    def create_memory(self):
-        M = Memory()
+    def create_memory(self, user_title=None):
+        m = Memory()
 
-        # require input for all memory variables
-        M.update_title()
-        M.update_keywords()
-        M.update_body()
+        # if user provided a title in program args, set memory title using their entry
+        if user_title: m.title = user_title
+        # otherwise prompt them for a title entry
+        else: m.update_title()
 
-        self.memories.setdefault(self._get_memory_id(), M)
+        m.update_keywords()
+        m.update_body()
+
+        self.memories.setdefault(self._get_memory_key(), m)
         self.altered = True
 
 
     def edit_memory(self, user_keywords):
-        memory_matches = self._memory_match(user_keywords)
-        m_id = self._select_memory_from_list(memory_matches)
-        self.memories[m_id].edit_menu()
-        print('Successfully edited \'{}\''.format(self.memories[m_id].title))
+        m_matches = self._memory_match(user_keywords)
+        m_key = self._select_memory_from_list(m_matches)
+        self.memories[m_key].edit_menu()
+        print('Successfully edited \'{}\''.format(self.memories[m_key].title))
         self.altered = True
 
-    def _get_memory_id(self):
-        next_id = min(self.memory_ids)
-        self.memory_ids.remove(next_id)
+    def _get_memory_key(self):
+        print(self.memory_keys)
+        next_key = min(self.memory_keys)
+        self.memory_keys.remove(next_key)
 
-        #
-        if len(self.memory_ids) == 0:
-            self.memory_ids.add(next_id+1)
+        if len(self.memory_keys) == 0:
+            self.memory_keys.add(next_key + 1)
 
-        print(next_id, self.memory_ids)
-        return next_id
+        print(next_key, self.memory_keys)
+        return next_key
 
     def _memory_match(self, user_keywords):
-
+        """returns a list of (memory_key, memory_obj) tuples sorted in ascending order by relevancy to user_keywords"""
         user_keywords = set(standardize(user_keywords))
 
-        for memory in self.memories.values():
-            memory_keywords = memory.make_set()
-            memory.search_score = len(user_keywords.intersection(memory_keywords))
-
-        # memories are sorted by search score
-        matches = sorted(self.memories.items(), key=lambda x: x[1])
-
-        # reset search score for all memories
         for m in self.memories.values():
-            m.search_score=0
+            m_keywords = m.make_set()
+            m.search_score = len(user_keywords.intersection(m_keywords))
 
-        return matches
+        # m_matches is a list of (m_key, m_obj) tuples sorted in ascendaing order by m_obj.search_score
+        m_matches = sorted(self.memories.items(), key=lambda x: x[1])
+
+        # reset search score for all memories so they are scored correctly next time
+        for m in self.memories.values():
+            m.search_score = 0
+
+        return m_matches
 
     def recall_memory(self, user_keywords):
-        memory_matches = self._memory_match(user_keywords)
-        [print('{}\n\t{}'.format(m[1].title, m[1].body)) for m in memory_matches]
+        m_matches = self._memory_match(user_keywords)
+        m_key = self._select_memory_from_list(m_matches)
+        print('{}\n\t{}'.format(self.memories[m_key].title, self.memories[m_key].body))
 
     def remove_memory(self, user_keywords):
-        memory_matches = self._memory_match(user_keywords)
-        m_id = self._select_memory_from_list(memory_matches)
-        del self.memories[m_id]
+        m_matches = self._memory_match(user_keywords)
+        m_key = self._select_memory_from_list(m_matches)
+        del self.memories[m_key]
         self.altered = True
 
-    def _select_memory_from_list(self, memory_matches):
-        [print('{}){}'.format(i, m.title)) for i,m in memory_matches]
-        selection = input('> ')
-        if valid_input(selection):
-            selection = int(selection)
-            if selection in self.memories:
-                return selection
+    def _select_memory_from_list(self, m_matches):
+        [print('{}) {}'.format(i, m.title)) for i,m in m_matches]
+        print('x) Exit')
+        while True:
+            selection = input('> ')
+            if selection == 'x':
+                exit()
+            elif valid_input(selection):
+                selection = int(selection)
+                if selection in self.memories:
+                    return selection
+                else:
+                    print('Invalid memory selection \'{}\''.format(selection))
             else:
-                print('Invalid memory id \'{}\''.format(selection))
-        else:
-            print('Invalid entry \'{}\''.format(selection))
+                print('Invalid entry \'{}\''.format(selection))
 
 class Memory:
     def __init__(self):
@@ -130,26 +138,26 @@ class Memory:
         self.body = default_input('Body: ', self.body)
     def edit_menu(self):
         while True:
-            print('0) Edit Title\n1) Edit Keywords\n2) Edit Body\n3) Done')
+            print('1) Edit Title\n2) Edit Keywords\n3) Edit Body\n4) Done')
             selection = input('> ')
 
             if valid_input(selection):
                 selection = int(selection)
                 options = {
-                    0:self.update_title,
-                    1:self.update_keywords,
-                    2:self.update_body,
+                    1:self.update_title,
+                    2:self.update_keywords,
+                    3:self.update_body,
                 }
 
-                if selection == 3:
+                if selection == 4:
                     if len(input('Press ENTER to Confirm')) == 0:
                         return
                 else:
                     options[selection]()
 
     def make_set(self):
-        memory_data = ' '.join([self.title, self.keywords, self.body])
-        return set(standardize(memory_data))
+        m_data = ' '.join([self.title, self.keywords, self.body])
+        return set(standardize(m_data))
 
 def read_pkl(pkl_file):
     try:
@@ -184,8 +192,9 @@ def main(argv):
     user_keywords = ' '.join(argv['<keyword>'])
 
     if argv['--add']:
-        brain.create_memory()
-    elif argv['--rm']:
+        # assumed that any keywords entered would be the memory title if user intent is to add a new memory
+        brain.create_memory(user_keywords)
+    elif argv['--remove']:
         brain.remove_memory(user_keywords)
     elif argv['--edit']:
         brain.edit_memory(user_keywords)
