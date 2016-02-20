7460bb1a4bc00ced64dbfe1c6adce8034ec34d90
evanhenri <nonce@protonmail.ch>
2016-02-19 23:26:53 -0500

    Added --force and changed CLI keywords

--- memfog.py
+++ memfog.py
@@ -1,18 +1,15 @@
 """
-
-Usage: memfog [--add]
-       memfog [--remove <keyword>...] [--top <n>]
-       memfog [--export <dir_path>]
-       memfog [(--import <file_path>)]
+Usage: memfog add
+       memfog remove [--top <n> <keyword>...]
+       memfog import [--force] <file_path>
+       memfog export [<dir_path>]
+       memfog [--top <n> <keyword>...]
 
 Options:
   -h --help     Show this screen
   -v --version  Show version
-  -a --add      Create new memory record
-  -r --remove   List records containing keywords and remove selected
   -t --top <n>  Limit results to top n memories [default: 10]
-  -e --export   Export memory records to json file
-  -i --import   Load memories from json file
+  -f --force    Overwrite existing memories with imported memories if same title
 
 """
 from docopt import docopt
@@ -22,7 +19,6 @@ from src import fs, data, brain
 
 class Config:
     def __init__(self, root_dir):
-        self.top_n = 10
         self.data_dir = 'datadir/'
         self.mem_db = 'memories.db'
         self.body_dir = 'body/'
@@ -37,10 +33,14 @@ class Config:
         fs.init_dir(self.body_dir_path)
         fs.init_file(self.exclusions_file_path)
 
+        self.top_n = 10
+        self.force_import = False
+
 def main(argv):
     root_dir = os.path.dirname(os.path.realpath(__file__)) + '/'
     Conf = Config(root_dir)
 
+    Conf.force_import = argv['--force']
     top_n = argv['--top']
     if top_n:
         if data.is_valid_input(top_n): Conf.top_n = int(top_n)
@@ -51,13 +51,13 @@ def main(argv):
     # delimit words with whitespace so they can be processed at same time as memory string data
     user_input = ' '.join(argv['<keyword>'])
 
-    if argv['--add']:
+    if argv['add']:
         Brain.create_mem()
-    elif argv['--remove']:
+    elif argv['remove']:
         Brain.remove_mem(user_input)
-    elif argv['--export']:
+    elif argv['export']:
         Brain.export_mem(argv['<dir_path>'])
-    elif argv['--import']:
+    elif argv['import']:
         Brain.import_mem(argv['<file_path>'])
     elif len(Brain.memories) > 0:
         Brain.display_mem(user_input)
