e75ca1b82cef7abfe1c7037186b68a8b89846721
evanhenri <nonce@protonmail.ch>
2016-03-03 23:08:33 -0500

    initial commit
    Link class adds functionality to embed a link in a new Record using [LINK_TYPE](PATH) syntax. Whenever the Record is retrieved, the content at the linked path is displayed as the body text. Current support for local filesystem paths and Github Gists

---
+++ src/link.py
@@ -0,0 +1,32 @@
+import re
+import github3
+
+from . import file_io, file_sys
+
+class Link:
+    def __init__(self):
+        keys = '|'.join(['GIST','PATH'])
+        # capture key and value when found in [key](value) format and key is in keys
+        self.prog = re.compile('(?:\[)('+keys+')(?:\]\()(.*?)(?:\))')
+
+    def expand(self, s):
+        for match in self.prog.finditer(s):
+            key, value = match.groups()
+
+            if key == 'PATH':
+                # replace [PATH](fp) in Record.body with the contents of the file at fp
+                #s = self.prog.sub(r'\g<0>', 'txt') #file_io.str_from_file(value))
+                s = s.replace(match.group(0), file_io.str_from_file(value))
+
+            elif key == 'GIST':
+                gist = github3.gist(value)
+                g = ''.join([str(f.content()) for f in gist.files()])
+                s = s.replace(match.group(0), g)
+
+        return s
+
+
+
+
+
+
