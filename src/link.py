ef96abc96e6a8f556b1a7997215388b0b5b77f5f
evanhenri <nonce@protonmail.ch>
2016-03-21 12:00:25 -0400

    Removed functionality to link to github gists
    This feature is too specialized as it caters to a specific service. Removed it to prevent reliance on that service

--- src/link.py
+++ src/link.py
@@ -1,11 +1,10 @@
 import re
-import github3
 
 from . import file_io
 
 class Link:
     def __init__(self):
-        keys = '|'.join(['GIST','PATH'])
+        keys = '|'.join(['PATH'])
         # capture key and value when found in [key](value) format and key is in keys
         self.prog = re.compile('(?:\[)('+keys+')(?:\]\()(.*?)(?:\))')
 
@@ -16,10 +15,5 @@ class Link:
             if key == 'PATH':
                 # replace [PATH](fp) in Record.body with the contents of the file at fp
                 s = s.replace(match.group(0), file_io.str_from_file(value))
-
-            elif key == 'GIST':
-                gist = github3.gist(value)
-                g = ''.join([str(f.content()) for f in gist.files()])
-                s = s.replace(match.group(0), g)
 
         return s
