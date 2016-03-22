6abc97d49142c453aba8c1aa31cd436031976178
evanhenri <nonce@protonmail.ch>
2016-03-21 22:44:09 -0400

    Renamed variable

--- src/link.py
+++ src/link.py
@@ -6,10 +6,10 @@ class Link:
     def __init__(self):
         keys = '|'.join(['PATH'])
         # capture key and value when found in [key](value) format and key is in keys
-        self.prog = re.compile('(?:\[)('+keys+')(?:\]\()(.*?)(?:\))')
+        self._pattern = re.compile('(?:\[)('+keys+')(?:\]\()(.*?)(?:\))')
 
     def expand(self, s):
-        for match in self.prog.finditer(s):
+        for match in self._pattern.finditer(s):
             key, value = match.groups()
 
             if key == 'PATH':
