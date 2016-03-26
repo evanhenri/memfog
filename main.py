39fff62889ee00ab64b9dc2f0ec1aa196ad39b4e
evanhenri <nonce@protonmail.ch>
2016-03-26 13:52:46 -0400

    Updated docopt tag

--- main.py
+++ main.py
@@ -223,5 +223,5 @@ def main(argv):
         print('No memories exist')
 
 if __name__ == '__main__':
-    args = docopt(__doc__, version='memfog v1.6.2')
+    args = docopt(__doc__, version='memfog v1.6.2.1')
     main(args)
