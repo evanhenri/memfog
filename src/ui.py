131dba74831d0c1d691a05c39c115ef3d051ad10
evanhenri <nonce@protonmail.ch>
2016-04-28 02:19:32 -0400

    Program now uses multiprocessing to allow for background operations while UI is being shown
    UI runs in parent process and database operations are currently the only thing running in a child process. Interaction is done using a queue where UI methods put Context objects into the queue which the ProcessHandler uses to perform database operations.

--- src/ui.py
+++ src/ui.py
@@ -163,12 +163,12 @@ class Content(urwid.ListBox):
 
     def keyword_widget_handler(self):
         interaction_mode = self['header']['label'].text
-        switch = { 'INSERT':self._show_keywords, 'COMMAND':self._hide_keywords }
+        switch = { 'INSERT':self.show_keywords, 'COMMAND':self.hide_keywords }
         switch[interaction_mode]()
 
-    def _show_keywords(self):
+    def show_keywords(self):
         self.base_widget.body.insert(1, self.keyword_widget)
-    def _hide_keywords(self):
+    def hide_keywords(self):
         if len(self.base_widget.body) == 4:
             self.keyword_widget = self.base_widget.body.pop(1)
 
@@ -224,7 +224,7 @@ class WidgetController(urwid.Frame):
             'body':self['body'].edit_text
         }
 
-    def update(self, data={}):
+    def set_widget_text(self, data={}):
         if 'interaction_mode' in data:
             self['header']['label'].set_text(data['interaction_mode'])
             self.footer.set_mode(data['interaction_mode'])
@@ -237,81 +237,81 @@ class WidgetController(urwid.Frame):
 
 
 class DataController:
-    def __init__(self, recv_queue, send_queue, record):
-        self.recv_queue = recv_queue
-        self.send_queue = send_queue
-
+    def __init__(self, record):
         self.data = Data(record)
-
         self.interaction_mode = ''
         self.view_mode = ''
 
     def dump(self):
-        return { 'RAW':self.get('RAW'), 'INTERPRETED':self.get('INTERPRETED') }
+        return { 'RAW':self.get_view('RAW'), 'INTERPRETED':self.get_view('INTERPRETED') }
 
-    def get(self, view_mode):
+    def get_view(self, view_mode):
         switch = { 'RAW':self.data.raw.dump, 'INTERPRETED':self.data.interpreted.dump }
         return { **switch[view_mode](), **{'interaction_mode':self.interaction_mode} }
 
-    def update(self, view_data):
+    def save_view(self, view_data):
         """
         Updates stored data to reflect view_data from Widgets
         If record data is not being interpreted, makes sure data from other views is kept in sync
         """
         if self.view_mode == 'RAW':
-            self.data.raw.update(view_data)
+            self.data.raw.update_fields(view_data)
             if not self.data.is_interpreted:
                 self.data.interpreted = self.data.raw
 
         elif self.view_mode == 'INTERPRETED':
-            self.data.interpreted.update(view_data)
+            self.data.interpreted.update_fields(view_data)
             if not self.data.is_interpreted:
                 self.data.raw = self.data.interpreted
 
 
 class UI:
-    def __init__(self, recv_queue, send_queue, record, interaction_mode='COMMAND', view_mode='INTERPRETED'):
-        signal.signal(signal.SIGINT, self._ctrl_c_callback)
+    def __init__(self, context, msg_queue):
+        signal.signal(signal.SIGINT, self.ctrl_c_callback)
+        self.exit_flag = False
+
+        self.context = context
+        self.msg_queue = msg_queue
 
         self.ScreenC = ScreenController()
-        self.DataC = DataController(recv_queue, send_queue, record)
+        self.DataC = DataController(context.record)
         self.WigetC = WidgetController()
 
-        self._set_interaction_mode(interaction_mode)
-        self._set_view_mode(view_mode)
+        self.set_interaction_mode(context.interaction_mode)
+        self.set_view_mode(context.view_mode)
 
-        self._exit_flag = False
-        self.db_update_required = False
+        self.ScreenC.run_wrapper(self.run)
 
-        self.ScreenC.run_wrapper(self._run)
-
-    def _set_interaction_mode(self, mode_id):
+    def set_interaction_mode(self, mode_id):
         self.DataC.interaction_mode = mode_id
         self.ScreenC.set_palette_mode(mode_id)
-        self.WigetC.update({'interaction_mode':mode_id})
+        self.WigetC.set_widget_text({'interaction_mode':mode_id})
         self.WigetC.body.keyword_widget_handler()
 
-    def _set_view_mode(self, mode_id):
+    def set_view_mode(self, mode_id):
         # Save text from current view before changing widget text
-        self.DataC.update(self.WigetC.dump())
+        self.DataC.save_view(self.WigetC.dump())
         self.DataC.view_mode = mode_id
-        self.WigetC.update(self.DataC.get(mode_id))
+        self.WigetC.set_widget_text(self.DataC.get_view(mode_id))
 
-    def _safe_exit(self):
-        self._exit_flag = True
-        self.DataC.update(self.WigetC.dump())
-        rec = self.DataC.data.get_altered_fields()
+    def safe_exit(self):
+        self.exit_flag = True
+        self.DataC.save_view(self.WigetC.dump())
 
         if self.DataC.data.is_interpreted:
-            self.DataC.data.interpreted.update_sources()
+            self.DataC.data.update_interpreted_sources()
 
-        context_flag = self.DataC.recv_queue.get()
-        self.DataC.send_queue.put(context_flag, self.DataC.data.rec_id, rec)
+        context = self.DataC.data.update_record_context(self.context)
+        self.msg_queue.put(context)
 
-    def _ctrl_c_callback(self, sig, frame):
-        self._safe_exit()
+        # Block until context put into queue is fully processed.
+        # Race condition occurs when adding new records if not present
+        self.msg_queue.join()
 
-    def _export(self, fp, content):
+    def ctrl_c_callback(self, sig, frame):
+        self.safe_exit()
+
+    def export(self, fp, content):
         default_dp = os.getcwd()
         default_fn = (content['title']).replace(' ', '_')
         fp = file_sys.fix_path(fp, default_dp, default_fn)
@@ -321,7 +321,7 @@ class UI:
         file_io.json_to_file(fp, self.WigetC.dump())
         return 'Exported to \'{}\''.format(fp)
 
-    def _evaluate_command(self, cmd_text):
+    def evaluate_command(self, cmd_text):
         if len(cmd_text) > 0:
             self.WigetC['footer'].cmd_history.append(cmd_text)
 
@@ -335,7 +335,7 @@ class UI:
                 if cmd == ':e' or cmd == ':export':
                     # If no filename/path was given, clear string to trigger default to be set in _export()
                     if args.startswith(cmd): args = ''
-                    result = self._export(args, self.WigetC.dump())
+                    result = self.export(args, self.WigetC.dump())
                     self.WigetC['footer'].set_edit_text(result)
 
                 elif cmd == ':h' or cmd == ':help':
@@ -343,14 +343,14 @@ class UI:
 
                 elif cmd == ':i' or cmd == ':insert':
                     self.WigetC['footer'].set_edit_text('')
-                    self._set_interaction_mode('INSERT')
+                    self.set_interaction_mode('INSERT')
 
                 elif cmd == ':q' or cmd == ':quit':
-                    self._safe_exit()
+                    self.safe_exit()
 
                 elif cmd == ':v' or cmd == ':view':
                     try:
-                        self._set_view_mode(args.upper())
+                        self.set_view_mode(args.upper())
                     except KeyError:
                         self.WigetC['footer'].set_edit_text('Valid views = \'raw\',\'interpreted\''.format(args))
                 else:
@@ -359,10 +359,10 @@ class UI:
                 self.WigetC['footer'].set_edit_text('Invalid command')
 
 
-    def _run(self):
+    def run(self):
         size = self.ScreenC.get_cols_rows()
 
-        while not self._exit_flag:
+        while not self.exit_flag:
             canvas = self.WigetC.render(size, focus=True)
             self.ScreenC.draw_screen(size, canvas)
             keys = None
@@ -375,14 +375,14 @@ class UI:
                     size = self.ScreenC.get_cols_rows()
 
                 elif k == 'ctrl c':
-                    self._safe_exit()
+                    self.safe_exit()
 
                 elif k in self.ScreenC.scroll_actions and self.WigetC.focus_position == 'body':
                     self.WigetC.keypress(size, k)
 
                 elif self.DataC.interaction_mode == 'INSERT':
                     if k == 'esc':
-                        self._set_interaction_mode('COMMAND')
+                        self.set_interaction_mode('COMMAND')
                     else:
                         self.WigetC.keypress(size, k)
 
@@ -392,7 +392,7 @@ class UI:
                     elif k == 'enter':
                         cmd_text = self.WigetC['footer'].get_edit_text()
                         self.WigetC['footer'].clear_text()
-                        self._evaluate_command(cmd_text)
+                        self.evaluate_command(cmd_text)
                         # if error results from entered command, clear error message before next keystroke appears
                         self.WigetC['footer'].clear_before_keypress = True
                     elif k == 'shift up':
